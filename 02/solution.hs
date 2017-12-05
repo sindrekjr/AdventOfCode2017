d3ed008307d92c0f008d461b0296f30cd10c919e
sindrekjr <sindre@sindrekjr.no>
2017-12-05 12:21:44 +0100

    Day 2 SOLVED!

--- 02/solution.hs
+++ 02/solution.hs
@@ -1,10 +1,6 @@
-
-readInt :: String -> Int
-readInt = read
-
-digits :: Int -> [Int]
-digits = map (read . (:[])) . show
+readInt :: [String] -> [[Int]]
+readInt = map (map read . words)
 
 main = do
-      rows <- return . map digits . map readInt . words =<< readFile "input"
+      rows <- return . readInt . lines =<< readFile "input"
       print (sum [ maximum(x) - minimum(x) | x <- rows ])
