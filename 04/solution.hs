da4644968bc355c572e9008e8e9720bbeb816fe4
sindrekjr <sindre@sindrekjr.no>
2017-12-05 17:41:04 +0100

    Day 4 solved

---
+++ 04/solution.hs
@@ -0,0 +1,8 @@
+import Data.List (sort, nub, tails, uncons)
+
+unique :: (Eq a) => [a] -> Bool
+unique = not . any (maybe False (uncurry elem) . uncons) . tails
+
+main = do
+      phrases <- return . nub . filter unique . map words . lines =<< readFile "input"
+      print (length phrases)
