d680a53762f679bacd1e329a59adb415971ae8ad
sindrekjr <sindre@sindrekjr.no>
2017-12-25 16:39:27 +0100

    Solved Day 19: Part 2

--- 19/solution.py
+++ 19/solution.py
@@ -7,13 +7,20 @@ with open("input", "r") as f:
 directions = { 'n': (-1,0), 's': (1,0), 'w': (0,-1), 'e': (0,1) }
 d = 's'
 
-result = ""
-y = 0; x = 0;
-while lines[y][x] == " ": x += 1
+res = ""
+steps = 0
+y = 0; x = 0
+while lines[y][x] == ' ': x += 1
 while True:
+    steps += 1
     while lines[y][x] != '+':
-        if lines[y][x] in letters: result += lines[y][x]
+        if lines[y][x] in letters: res += lines[y][x]
+        if len(res) == len(letters):
+            print("Part 1: {}".format(res) + "\nPart 2: {}".format(steps))
+            exit(0)
         y += directions[d][0]; x += directions[d][1]
+        steps += 1
+
     if d in "ns":
         if lines[y + directions['w'][0]][x + directions['w'][1]] != ' ': d = 'w'
         else: d = 'e'
@@ -21,7 +28,4 @@ while True:
         if lines[y + directions['n'][0]][x + directions['n'][1]] != ' ': d = 'n'
         else: d = 's'
 
-    if len(result) == len(letters): break
     y += directions[d][0]; x += directions[d][1]
-
-print(result)
