915ef8bbdf109575926a2dd4f6dd3fb46e61718f
sindrekjr <sindre@sindrekjr.no>
2018-01-12 19:29:30 +0100

    Small changes to Day 10

--- 10/solution.hs
+++ 10/solution.hs
@@ -15,6 +15,10 @@ knit list input i skip
       rnd = if (i + len) - (length list) < 0 then 0 else (i + len) - (length list)
       nl = take rnd (drop (len - rnd) rev) ++ take (i - rnd) (drop rnd list) ++ take (len - rnd) rev ++ drop (i + len) list
 
+ascList inp = inp ++ [17, 31, 73, 47, 23]
+sparseHash inp = knit [0..255] (take (64 * length asc) (cycle asc)) 0 0 where asc = ascList inp
+denseHash inp = map (foldl' xor 0) (unfoldr (bool (Just . splitAt 16) (const Nothing) =<< null) (sparseHash inp))
+
 part1 = do
   input <- return . map (read :: String -> Int) . splitOn "," =<< readFile "input"
   let result = knit [0..255] input 0 0
@@ -22,7 +26,4 @@ part1 = do
 
 part2 = do
   input <- return . map ord =<< readFile "input"
-  let ascList = input ++ [17, 31, 73, 47, 23]
-  let sparseHash = knit [0..255] (take (64 * length ascList) (cycle ascList)) 0 0
-  let denseHash = map (foldl' xor 0) (unfoldr (bool (Just . splitAt 16) (const Nothing) =<< null) sparseHash)
-  print $ concatMap (flip showHex "") denseHash
+  print $ concatMap (flip showHex "") $ denseHash input
